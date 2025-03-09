afc545d2ac769cc2d499136c3efea3b006910c33
aboulabed <suhailattackontitanahmed@gmail.com>
2025-03-09 14:35:01 +0200

    feat:add filter products logic

--- shop.php
+++ shop.php
@@ -1,3 +1,44 @@
+<?php
+include('server/connection.php');
+if (isset($_POST['search']) && isset($_POST['category'])) {
+    $category = $_POST['category'];
+
+    $price = $_POST['price'];
+    $stmt1 = $conn->prepare("SELECT * FROM products WHERE product_category = ? AND product_price BETWEEN 0 AND ? ");
+    $stmt1->bind_param("si", $category, $price);
+    $stmt1->execute();
+    $products = $stmt1->get_result();
+    $stmt1->close();
+} else {
+
+    $stmt1 = $conn->prepare("SELECT * FROM products ORDER BY product_category DESC");
+    $stmt1->execute();
+    $products = $stmt1->get_result();
+    $stmt1->close();
+}
+
+$stmt2 = $conn->prepare("SELECT DISTINCT product_category FROM products ");
+$stmt2->execute();
+$products_category = $stmt2->get_result();
+$stmt2->close();
+
+// Prepare the SQL query
+
+$stmt3 = $conn->prepare("SELECT MIN(product_price) AS lowest_price, MAX(product_price) AS highest_price FROM products");
+$stmt3->bind_result($lowest_price, $highest_price);
+$stmt3->execute();
+$stmt3->fetch();
+$stmt3->close();
+$highest_price = ceil($highest_price);
+$lowest_price = ceil($lowest_price);
+?>
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -37,14 +78,14 @@
                         <a class="nav-link" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="shop.html">Shop</a>
+                        <a class="nav-link active" href="shop.php">Shop</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="contact-us.html">Contact Us</a>
+                        <a class="nav-link" href="contact-us.php">Contact Us</a>
                     </li>
                     <li class="nav-item">
-                        <a href="cart.html"><i class="fas fa-shopping-cart " style="color:#212529 
+                        <a href="cart.php"><i class="fas fa-shopping-cart " style="color:#212529 
 "></i></a>
 
                         <a href="account.php"><i class="fas fa-user" style="color:#212529"></i></a>
@@ -53,51 +94,34 @@
             </div>
         </div>
     </nav>
-    <div class="d-flex shop justify-content-between">
+    <div class="d-flex shop justify-content-between" style="width: 90%;">
         <!-- Filter Products -->
-        <section id="search" class="my-5 py-5 ms-2 	d-none d-lg-block " style="width: 20%;">
+        <section id="search" class="my-5 py-5 ms-2 ps-2	d-none d-lg-block " style="width: 20%;">
             <div class="container mt-5 py-5">
                 <p>Search For Products</p>
                 <hr>
             </div>
-            <form action="">
+            <form action="shop.php" method="post">
                 <div class="row mx-auto container">
                     <div class="col-lg-12 col-md-12 col-sm-12">
                         <p>Category</p>
-
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="category" id="category1">
-                            <label class="form-check-label" for="category1">
-                                Bags
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="category" id="category2">
-                            <label class="form-check-label" for="category2">
-                                Pants
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="category" id="category3">
-                            <label class="form-check-label" for="category3">
-                                Hats
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="category" id="category4">
-                            <label class="form-check-label" for="category4">
-                                Shoes
-                            </label>
-                        </div>
+                        <?php while ($category = $products_category->fetch_assoc()) { ?>
+                            <div class="form-check">
+                                <input class="form-check-input" value="<?php echo $category['product_category']; ?>" type="radio" name="category" id="category1" required> 
+                                <label class="form-check-label" for="category1">
+                                    <?php echo $category['product_category']; ?>
+                                </label>
+                            </div>
+                        <?php } ?>
                     </div>
                 </div>
                 <div class="row mx-auto container mt-5">
                     <div class="col-lg-12 col-md-12 col-sm-12">
                         <p>Price</p>
-                        <input type="range" class="form-range w-50" min="1" max="1000" id="priceRange">
+                        <input type="range" class="form-range w-50" name="price" value="20" min="<?php echo $lowest_price; ?>" max="<?php echo $highest_price; ?>" id="priceRange" required>
                         <div class="w-50">
-                            <p id="priceValue" class="float-start">1</p>
-                            <p id="priceValue" class="float-end">1000</p>
+                            <p id="priceValue" class="float-start">$<?php echo $lowest_price; ?></p>
+                            <p id="priceValue" class="float-end">$<?php echo $highest_price; ?></p>
 
                         </div>
                     </div>
@@ -108,8 +132,8 @@
                 </div>
             </form>
         </section>
-        <!-- Featured  -->
-        <section id="featured" class="my-5 py-5">
+        <!-- Products  -->
+        <section id="products" class="my-5 py-5">
             <div class="container text-center mt-5 py-5">
                 <h3>Products Collection</h3>
                 <hr class="mx-auto" />
@@ -117,214 +141,22 @@
 
             </div>
             <div class="row mx-auto container ">
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg" alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star-half-alt"></i>
+                <?php while ($product = $products->fetch_assoc()) { ?>
+                    <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
+                        <img src="assets/imgs/<?php echo $product['product_image']; ?>" alt="" class="img-fluid mb-3 img-thumbnail">
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                        <h5 class="p-name"><?php echo $product['product_name']; ?></h5>
+                        <h4 class="p-price">$<?php echo $product['product_price']; ?></h4>
+                        <a href="single-product.php?product_id=<?php echo $product["product_id"] ?>"><button class="buy-btn">Buy Now</button></a>
+
                     </div>
-                    <h5 class="p-name">White T-Shirt</h5>
-                    <h4 class="p-price">$19</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg" alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h5 class="p-name">Sports Bag</h5>
-                    <h4 class="p-price">$9</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg " alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star-half-alt"></i>
-                    </div>
-                    <h5 class="p-name">Brown Trouser</h5>
-                    <h4 class="p-price">$29</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg " alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h5 class="p-name">Classic Wallet</h5>
-                    <h4 class="p-price">$49</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg" alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star-half-alt"></i>
-                    </div>
-                    <h5 class="p-name">White T-Shirt</h5>
-                    <h4 class="p-price">$19</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg" alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h5 class="p-name">Sports Bag</h5>
-                    <h4 class="p-price">$9</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg " alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star-half-alt"></i>
-                    </div>
-                    <h5 class="p-name">Brown Trouser</h5>
-                    <h4 class="p-price">$29</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg " alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h5 class="p-name">Classic Wallet</h5>
-                    <h4 class="p-price">$49</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg" alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star-half-alt"></i>
-                    </div>
-                    <h5 class="p-name">White T-Shirt</h5>
-                    <h4 class="p-price">$19</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg" alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h5 class="p-name">Sports Bag</h5>
-                    <h4 class="p-price">$9</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg " alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star-half-alt"></i>
-                    </div>
-                    <h5 class="p-name">Brown Trouser</h5>
-                    <h4 class="p-price">$29</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg " alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h5 class="p-name">Classic Wallet</h5>
-                    <h4 class="p-price">$49</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg" alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star-half-alt"></i>
-                    </div>
-                    <h5 class="p-name">White T-Shirt</h5>
-                    <h4 class="p-price">$19</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg" alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h5 class="p-name">Sports Bag</h5>
-                    <h4 class="p-price">$9</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg " alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star-half-alt"></i>
-                    </div>
-                    <h5 class="p-name">Brown Trouser</h5>
-                    <h4 class="p-price">$29</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
-                <div class="product mb-5 text-center col-lg-3 col-md-4 col-sm-12">
-                    <img src="assets/imgs/bag-1.jpg " alt="" class="img-fluid mb-3 img-thumbnail">
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h5 class="p-name">Classic Wallet</h5>
-                    <h4 class="p-price">$49</h4>
-                    <button class="buy-btn">Buy Now</button>
-                </div>
+                <?php } ?>
 
                 <nav aria-label="Page navigation example">
                     <ul class="pagination justify-content-center">
